e7fed78ca5b2ceb1840ea1ea5551636183175241
mihake <mihake@163.com>
2022-08-23 17:21:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# 微信测试号之每日早安推送给对象
+
+## 须知
+ 本项目对 https://github.com/rxrw/daily_morning 项目进行二次代码补充，原作者目前正在更新。请移步。二创是因想添加点更多的东西进去自用，无商业行为，侵删。
+
+## 模板
+☀𝒢𝑜𝑜𝒹 𝑀𝑜𝓇𝓃𝒾𝓃𝑔
+———————૮₍ ˃ ⤙ ˂ ₎ა———————
+亲爱的XX宝宝，早上好呀｡◕ᴗ◕｡
+今天是我们在一起的第{{love_days.DATA}}天啦
+
+💭〰️️💕〰️💭〰️️💕〰️💭〰️💕〰️💭
+宝宝的专属XX市天气提醒：
+『{{date.DATA}} | {{week.DATA}}』
+️🕊️【最高气温：{{min_temperature.DATA}}】
+🦢【最低气温：{{max_temperature.DATA}}】
+☀️【当前气温：{{now_temperature.DATA}}】
+🔭【紫外线等级：{{uv_index.DATA}}】
+🐳【降雨概率：{{rain_odds.DATA}}%】
+🍭【今日:{{weather.DATA}}】
+💞【我们距离:1601.8公里】
+🔔XX温馨提示：{{weather_tips.DATA}}
+
+😙宝宝今年{{age.DATA}}岁了，距离生日还有{{birthday.DATA}}天，
+
+正在播放『爱你-王心凌』
+↻ ㅤㅤ           ㅤ◁           ㅤㅤ❚❚             ㅤㅤ▷ 
+━━━━━━━●───────
+
+🥪要开心嗷,记得吃早餐么么~~
+
+🍀【{{words.DATA}}】
---
+++ main.py
@@ -0,0 +1,76 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+user_ids = os.environ["USER_ID"].split("\n")
+template_id = os.environ["TEMPLATE_ID"]
+tx_key = os.environ["TX_KEY"]
+
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthday = os.environ['BIRTHDAY']
+
+today = datetime.now()
+
+def get_weather():
+  url = "http://api.tianapi.com/tianqi/index?key="+ tx_key +"&city=" + city +"市"
+  res = requests.get(url).json()
+  weather = res['newslist'][0]
+  newdate = weather['date']
+  return newdate, weather['week'], weather['weather'], weather['real'], weather['lowest'], weather['highest'], weather['pop'], weather['uv_index'], weather['tips'], 
+
+def get_count():
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+def get_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_words():
+  words = requests.get("https://api.shadiao.pro/chp")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+##
+def get_age():
+  delta = today - datetime.strptime('2001-'+ birthday, "%Y-%m-%d")
+  return int(delta.days/365)
+
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+newdate,week,weather,real,lowest,highest,pop,uv_index,tips = get_weather()
+data = {
+  "love_days":{"value":get_count()},
+  "date":{"value":newdate},
+  "week":{"value":week},
+  "min_temperature":{"value":lowest,},
+  "max_temperature": {"value":highest},
+  "now_temperature":{"value":real},
+  "rain_odds":{"value":pop},
+  "uv_index":{"value":uv_index},
+  "weather":{"value":weather},
+  "weather_tips":{"value":tips},
+  "age":{"value":get_age()},
+  "birthday":{"value":get_birthday()},
+  "words":{"value":get_words()},
+}
+count = 0
+for user_id in user_ids:
+  res = wm.send_template(user_id, template_id, data)
+  count+=1
+
+print("发送了" + str(count) + "条消息")
